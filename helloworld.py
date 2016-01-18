d1d7a74231336f0cb029694baa6a5afdabf1fdad
Bobby Gatlin <bgatlin@mail.valenciacollege.edu>
2016-01-18 00:32:41 -0500

    Done

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-# replace the contents of this comment with your full name
+# Bobby Gatlin
 
 # write a program that:
 # 1. greets the user in English
@@ -7,4 +7,33 @@
 # 4. exits
 
 # make sure that your code contains comments explaining your logic!
+
+
+#First I just want to setup everything with print
+print ('Hello World!')
+print ('')
+print ('Pick a language and I will greet you in that language')
+print ('1.Dutch')
+print ('2.Portuguese')
+print ('3.Italian') #The next thing I did was define a value to the variable c
+print('')
+c=int(input())
+if c==1:
+    print ('Hello wereld')
+if c==2:
+    print ("Olá mundo")
+if c==3:
+    print ("Ciao mondo")
+
+#That's it.
+#The code successfully greets with Hello World in Dutch, Portuguese, or Italian and then exits.
+    
+
+
+    
+
+
+       
+
+
 
